23609a713257b4801a15618fa101446ad4dcb2cc
Jethro Adebisi <adebisirevel@gmail.com>
2026-01-16 23:30:26 +0100

    Update TypeScript ESLint dependencies and fix import
    Upgrades @typescript-eslint/eslint-plugin and @typescript-eslint/parser to version 7.0.0 in package.json. Also removes accidental shell commands from the import statement in SwapWidget.tsx.

--- src/components/SwapWidget.tsx
+++ src/components/SwapWidget.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+rm -rf node_modules package-lock.json
+npm installrm -rf node_modules package-lock.json
+npm installimport React, { useState } from 'react';
 import { ChevronDown, X } from 'lucide-react';
 import { useCardanoWallet } from '../Providers';
 import { useTokenQuote, TradeType } from '../hooks/useTokenQuote';
